29f0bd4eb5ae3a6d892949c1d433c75693196c3b
Jack Ha <jackh@kth.se>
2012-04-13 13:27:53 +0200

    modified the TweetParser. First try to commit some small changes

--- TwitterPageRank/src/TweetParser.java
+++ TwitterPageRank/src/TweetParser.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -14,33 +17,57 @@ import com.twitter.Extractor;
 public class TweetParser {
 
 	// Constant (better put as an input parameter).
-	private static String path = ".";
+	private static String path = "C:\\Users\\Jack\\Dropbox\\KTH-stuff\\DD2476 Search Engines and Information Retrieval Systems\\DD2476 Search Engines and Information Retrieval Project\\EmilJack\\tweets";
 	
 	public static void main(String[] args) {
 		
 		// Variables.
-    	Extractor extractor = new Extractor();   // To extract @user, #hashtag ...
+    	Extractor extractor = new Extractor();   // To extract @user, #hashtag ...		
+    	File dokDir = new File(path);
+    	String[] fs = dokDir.list();
+		ArrayList<String> userTweets = new ArrayList<String>();
 		
-		// Read the file @user.txt.
-		String userTweets = loadUserTweets("Bernard_bahp");		
-		System.out.println(userTweets);
+		/** The graph as a hashtable. */
+	    HashMap<String,ArrayList<String>> index = new HashMap<String,ArrayList<String>>();
+	    
+	    HashMap<String, ArrayList<String>> index2 = new HashMap<String,ArrayList<String>>();
+		
+    	
+    	//Read all the files from /Users/ directory
+    	if (dokDir.canRead()){
+    		if (dokDir.isDirectory()){
+    			if ( fs != null ) {
+    			    for ( int i=0; i<fs.length; i++ ) {
+   			    	// Read the file @user.txt.
+    			    //System.out.println(fs[i]);	
+    			    	
+    			    userTweets.add(loadUserTweets(fs[i]).toString());
+    			    System.out.println(userTweets.get(i));
+    			    }
+    			}
+    		}
+    	}
+    	
+		//String userTweets = loadUserTweets("Bernard_bahp");
+    	//String userTweets = loadUserTweets(directory);
 			
 		// Jason array with JSon Object (each object would be one tweet).
-		try {
+    	for (int i=0; i<fs.length; i++){
+    		try {
 	         	JSONArray jsonArray;
-				jsonArray = new JSONArray(userTweets);
+				jsonArray = new JSONArray(userTweets.get(i));
 	            
 				System.out.print("Reading Bernard_bahp tweets.");
 	    	    System.out.println(" (Total : " + jsonArray.length() + ")");
 	    	 
 		    	// Look the @user references in each of the tweets.
 		    	List<String> namesReferenced;
-		    	for (int i = 0; i < jsonArray.length(); i++) {
+		    	for (int j = 0; j < jsonArray.length(); j++) {
 		    	
-		    		System.out.print("Tweet " + i + " : ");
+		    		System.out.print("Tweet " + j + " : ");
 		    		
-		    		// Get JSon Object (tweet) in position i.
-		    		JSONObject jsonObject = jsonArray.getJSONObject(i);
+		    		// Get JSon Object (tweet) in position j.
+		    		JSONObject jsonObject = jsonArray.getJSONObject(j);
 		    		
 		    		// Get the field "Text" in that tweet. (also are others "id", "created_at", ...").
 		    		namesReferenced = extractor.extractMentionedScreennames(jsonObject.getString("text"));
@@ -52,14 +79,16 @@ public class TweetParser {
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+    		
+    	} 
 	}	
 	
 	@SuppressWarnings("finally")
 	public static String loadUserTweets(String name){
 		String content = "";
 		try {
-			File file = new File(path + "/" + name + ".txt");
+			File file = new File(path + "/" + name);
 			
 			if (file.exists()){
 				BufferedReader br = new BufferedReader(new FileReader(file));
