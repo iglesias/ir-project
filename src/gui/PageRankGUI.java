15290709eb48a271df7db673351145a770d693f3
Bernard Hernandez <bernard.hernandez.perez@gmail.com>
2012-05-01 04:14:25 +0200

    ~ minor changes

--- src/gui/PageRankGUI.java
+++ src/gui/PageRankGUI.java
@@ -10,14 +10,17 @@ public class PageRankGUI {
 	/**
 	 * Static variables.
 	 */
-	public static String rootPath = "./TwitterPageRank/data/";    
+	public static String rootPath = "./data/";    
 	public static String pagerankMentionedPath = "/pagerank/mentioned/";
 	public static String pagerankRetweetedPath = "/pagerank/retweeted/";
-	public static String blackListPath = "/data/blacklist.txt";
+	public static String blackListPath = "./data/blacklist.txt";
+	public static String iconPath = "./images/twitter_bird.png";
+	
 	public static String actUniverse = null;
 	public static String actMethod = null;
+	public static String actRetrieved = "0";
 	
-	public static String iconPath = "";
+	public static String maxDocumentsRetrieved = "20";
 	
 	/**
 	 * Attributes.
