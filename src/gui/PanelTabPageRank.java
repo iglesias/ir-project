ed75b2582f46010fa09ade888bbf2de05c12bb5f
Bernard Hernandez <bernard.hernandez.perez@gmail.com>
2012-05-01 04:16:06 +0200

    ~ reordered JPanels

--- src/gui/PanelTabPageRank.java
+++ src/gui/PanelTabPageRank.java
@@ -20,7 +20,6 @@ public class PanelTabPageRank extends JPanel{
 	private PanelSelectionUniverse selectionUniverse;
 	private PanelSelectionMethod selectionMethod;
 	private PanelShowPageRank showPagerank;
-	private PanelBlackList showBlackList;
 
 	public PanelTabPageRank(){
 		
@@ -29,55 +28,47 @@ public class PanelTabPageRank extends JPanel{
 		// ----------------------------------------------------------------------------
 		selectionUniverse = new PanelSelectionUniverse();		
 		selectionMethod = new PanelSelectionMethod();
-		showBlackList = new PanelBlackList();
 		showPagerank = new PanelShowPageRank();
 		
 		// ----------------------------------------------------------------------------
 		//                        DEFINE LAYOUT ADMINISTRATOR
 		// ----------------------------------------------------------------------------
 		this.setLayout(new GridBagLayout());
-		selectionUniverse.setMinimumSize(new Dimension(50,50));
-		selectionMethod.setMinimumSize(new Dimension(50,50));
-		showBlackList.setMinimumSize(new Dimension(50,50));
-		
+		selectionUniverse.setSize(new Dimension(125,150));
+		selectionUniverse.setMinimumSize(new Dimension(125,150));
+		selectionMethod.setSize(new Dimension(125,150));
+		selectionMethod.setMinimumSize(new Dimension(125,150));
+
 		// ----------------------------------------------------------------------------
 		//                         DEFINE GRIDBAGCONSTRAINTS
 		// ----------------------------------------------------------------------------
 		GridBagConstraints infoUniverse = new GridBagConstraints();
 		infoUniverse.gridx = 0;
 		infoUniverse.gridy = 0;
-		infoUniverse.weightx = 1.0;
+		infoUniverse.weightx = 0.0;
 		infoUniverse.weighty = 0.0;
 		infoUniverse.fill = GridBagConstraints.BOTH;
 		
 		GridBagConstraints infoMethod = new GridBagConstraints();
-		infoMethod.gridx = 1;
-		infoMethod.gridy = 0;
-		infoMethod.weightx = 1.0;
+		infoMethod.gridx = 0;
+		infoMethod.gridy = 1;
+		infoMethod.weightx = 0.0;
 		infoMethod.weighty = 0.0;
 		infoMethod.fill = GridBagConstraints.BOTH;
 		
-		GridBagConstraints infoBlack = new GridBagConstraints();
-		infoBlack.gridx = 2;
-		infoBlack.gridy = 0;
-		infoBlack.weightx = 1.0;
-		infoBlack.weighty = 0.0;
-		infoBlack.fill = GridBagConstraints.BOTH;
-		
 		GridBagConstraints infoPagerank = new GridBagConstraints();
-		infoPagerank.gridx = 0;
-		infoPagerank.gridy = 1;
+		infoPagerank.gridx = 1;
+		infoPagerank.gridy = 0;
 		infoPagerank.weightx = 1.0;
 		infoPagerank.weighty = 1.0;
 		infoPagerank.fill = GridBagConstraints.BOTH;
-		infoPagerank.gridwidth = 3;
+		infoPagerank.gridheight = 2;
 
 		// ----------------------------------------------------------------------------
 		//                    ADD THE COMPONENTS TO THE CONTAINER
 		// ----------------------------------------------------------------------------
 		this.add(selectionUniverse,infoUniverse);
 		this.add(selectionMethod,infoMethod);
-		this.add(showBlackList,infoBlack);
 		this.add(showPagerank,infoPagerank);
 		
 		// ----------------------------------------------------------------------------
